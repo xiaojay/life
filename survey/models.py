cac38fc6ed6945ef0b254bc48a0277f75665b8ac
jay <xiaojay@gmail.com>
2015-03-26 09:35:39 +0800

    impove survey models

--- survey/models.py
+++ survey/models.py
@@ -10,4 +10,34 @@ class Algae(models.Model):
     def __unicode__(self):
         return '%s(%s)'%(self.chinese_name,self.name)
 
+class Survey(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    start = models.DateField(blank=True, null=True)
+    end = models.DateField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
 
+    def __unicode__(self):
+        return self.name
+
+class Site(models.Model):
+    survey = models.ForeignKey('Survey', )
+    name = models.CharField(max_length=100)
+    longitude = models.CharField(max_length=100, blank=True, null=True)
+    latitude =  models.CharField(max_length=100, blank=True, null=True)
+    
+    def __unicode__(self):
+        return self.name
+    
+class Sample(models.Model):
+    survey = models.ForeignKey('Survey')
+    site = models.ForeignKey('Site')
+    time = models.DateTimeField(blank=True, null=True)
+
+class AlgaeData(models.Model):
+    sample = models.ForeignKey('Sample')
+    algae = models.ForeignKey('Algae')
+    abundance = models.FloatField(blank=True, null=True)
+    abundance_percent = models.FloatField(blank=True, null=True)
+    biomass = models.FloatField(blank=True, null=True)
+    biomass_percent = models.FloatField(blank=True, null=True)
